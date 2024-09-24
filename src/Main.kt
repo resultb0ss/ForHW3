094cf37ab643248d8336c053c209c653a379ff9e
Admin <reteamboss@gmail.com>
2024-09-24 14:47:12 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+
+    val x1 = 10
+    val x2 = 20
+    val y1 = 10
+    val y2 = 40
+
+    val storTwo = y2 - y1
+    val storOne = x2 - x1
+
+    val P = (storOne + storTwo) * 2
+    val S = storOne * storTwo
+    println(P)
+    println(S)
 }
